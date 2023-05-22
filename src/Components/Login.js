6de5f1661fce113dcfcb72221bcd89eac67fe62f
Naitik Patel <naitik2002patel@gmail.com>
2023-05-22 11:27:30 +0530

    Day-3: .env file, add style variable to add CSS with MUI

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: 'cyan',
+    fontSize: '16px',
+    fontWeight: 'bold',
+  },
+  h2:{
+    textAlign: "center",
+  }
+});
 const Login = () => {
+  const classes = useStyles();
   return (
-    <div>
-      <h2 style={{textAlign:"center"}}>Login</h2>
+    <div className={classes.root}>
+      <h2 className={classes.h2}>Login</h2>
     </div>
   );
 };
--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,9 +1,15 @@
 import React from "react";
 
 const Products = () => {
+  const message = process.env.REACT_APP_MESSAGE;
+  const imageUrl = process.env.REACT_APP_IMAGE_URL;
   return (
     <div>
-      <h2 style={{textAlign:"center"}}>Products</h2>
+      <h2 style={{ textAlign: "center" }}>Products</h2>
+      <div>
+        <h1>{message}</h1>
+        <img src={imageUrl} alt="My Image" width={500}/>
+      </div>
     </div>
   );
 };
